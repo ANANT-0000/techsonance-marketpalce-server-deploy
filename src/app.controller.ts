86f32b5c127f3b8f85e316fd87e14026bee0aa6e
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2026-05-09 09:38:13 +0530

    refactor: simplify AppService constructor and remove unused routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,17 +4,8 @@ import express from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  @Get('/test')
+  @Get()
   getHello() {
     return this.appService.getHello();
-  }
-  @Get('/test-cookie')
-  getcookie(@Res({ passthrough: true }) res: express.Response) {
-    res.cookie('test_cookie', 'Hello from NestJS!', {
-      httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
-    });
-    return { message: 'Cookie has been set!' };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { DRIZZLE } from './drizzle/drizzle.module';
-import { type DrizzleDB } from './drizzle/types/drizzle';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject(DRIZZLE) private db: DrizzleDB) {}
+  constructor() {}
   getHello(): string {
     return 'Hello World!';
   }
