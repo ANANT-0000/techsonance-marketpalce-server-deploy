a52092f50fefabff38861a8df4114bc52d656917
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2026-06-28 14:27:27 +0530

    ..

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# stage 1
+FROM node:24-alpine AS builder
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+RUN npm ci
+COPY . .
+COPY .env ./
+
+RUN  npm run build
+RUN npm prune --production
+#stage 2
+FROM node:24-alpine AS runner
+WORKDIR /usr/src/app
+ENV NODE_ENV=production
+COPY --from=builder /usr/src/app/node_modules /usr/src/app/node_modules
+COPY --from=builder /usr/src/app/package*.json /usr/src/app/
+COPY --from=builder /usr/src/app/dist /usr/src/app/dist
+COPY --from=builder /usr/src/app/drizzle.config.ts /usr/src/app/drizzle.config.ts
+
+EXPOSE 8000
+CMD [ "npm","run","start:prod" ]
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,15 @@ import 'dotenv/config';
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined in the environment variables.');
 }
+const isDist = __dirname.includes('/dist') || __dirname.includes('\\dist');
+// If running from dist/drizzle.config.js, schema is likely at dist/src/drizzle/schema
+const schemaPath = isDist
+  ? './dist/src/drizzle/schema'
+  : './src/drizzle/schema';
+
 export default defineConfig({
-  schema: './src/drizzle/schema',
-  out: './src/drizzle/migrations',
+  schema: schemaPath,
+  out: isDist ? './dist/src/drizzle/migrations' : './src/drizzle/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
